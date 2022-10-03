20f148eb74f0f6e4224fb4e43c189023da6cfca9
Yunus Emre Topçu <yunusemre_topcu@hotmail.com>
2022-10-04 02:01:18 +0300

    Card component oluşturuldu style işlemleri yapıldı

---
+++ src/components/Card/Card.vue
@@ -0,0 +1,18 @@
+<script setup></script>
+
+<template>
+	<div class="card"></div>
+</template>
+
+<style scoped lang="scss">
+.card {
+	box-sizing: border-box;
+	height: 300px;
+	width: 495px;
+	background-color: #ffffff;
+	border: 1px solid #e5e5e5;
+	border-radius: 8px;
+	box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px,
+		rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
+}
+</style>

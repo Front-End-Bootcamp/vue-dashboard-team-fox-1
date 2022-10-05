c800857243a1583e8e413a27cf8748f28937cd57
Abdulkadir Celebi <abdulkadircelebi.ac@gmail.com>
2022-10-05 22:06:40 +0300

    CardEdit and CardProgress bar components created

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,23 @@
 <script setup>
-import BootcampLogo from "./assets/svg/BootcampLogo.vue"
-import HelloWorld from './components/HelloWorld.vue'
+import Card from './components/Card/Card.vue';
 </script>
 
 <template>
-	<div>
-		<BootcampLogo />
-		<br />
-		<a href="https://vitejs.dev" target="_blank">
-			<img src="/vite.svg" class="logo" alt="Vite logo" />
-		</a>
-		<a href="https://vuejs.org/" target="_blank">
-			<img src="/vue.svg" class="logo vue" alt="Vue logo" />
-		</a>
+	<div class="container">
+		<div class="cardContent"><Card ></Card></div>
+		<div class="cardContent"><Card ></Card></div>
 	</div>
-	<HelloWorld msg="Vite + Vue" />
+	
 </template>
 
-<style scoped>
-.logo {
-	height: 6em;
-	padding: 1.5em;
-	will-change: filter;
-}
+<style  lang="scss" scoped>
+	.container {
+		display: flex;
+		background: #E5E5E5;
+	}
+	.cardContent {
+		margin: 15px;
+	}
 
-.logo:hover {
-	filter: drop-shadow(0 0 2em #646cffaa);
-}
 
-.logo.vue:hover {
-	filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>

4f243024292d24a74be49bdc31456c8e7a9ba475
Abdulkadir Celebi <abdulkadircelebi.ac@gmail.com>
2022-10-05 22:07:51 +0300

    progress bar and card edit components created

---
+++ src/components/Card/CardProgressBar.vue
@@ -0,0 +1,52 @@
+<script setup>
+import {ref} from "vue"
+let percent = ref(parseFloat(Math.random()*100).toFixed(0))
+console.log(percent);
+</script>
+
+<template>
+	<div class="progressText"> Progress <span class="progressBarText" >{{percent}}%</span></div>
+		<div class="box">
+			<div class="progressBar" :style="{'width':percent+'%'}" >
+		</div>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+.box {
+	width: 100%;
+	height: 8px;
+	margin-right: 26px;
+	margin-top: 8px;
+	margin-bottom: 24px;
+	border-radius: 75px;
+	background:#F1F2F7;
+	box-shadow:  3px 3 px 30px 3px rgba(0, 0, 0, 0.1);
+	text-align: right;
+	align-items: center;
+}
+.progressBar {
+	width: 100%;
+	height: 8px;
+	margin-right: 26px;
+	margin-top: 8px;
+	margin-bottom: 24px;
+	border-radius: 75px;
+	background:#1BE3A7;
+	box-shadow:  3px 3 px 30px 3px rgba(0, 0, 0, 0.1);
+	text-align: right;
+	align-items: center;
+}
+.progressText{
+	margin-top: 206px;
+	color: black;
+	text-align: left;
+	font-size: 13px;
+
+}
+.progressBarText{
+	color: black;
+	float: right;
+	font-size: 12px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+import { faHatWizard } from '@fortawesome/free-solid-svg-icons';
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+library.add(faHatWizard);
+
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')

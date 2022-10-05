a9149504c5e77d4404c8ddffa9a5869a792f294f
mfurkanakin <mfakin@yahoo.com>
2022-10-05 22:15:53 +0300

    new created to status and date

---
+++ src/components/Card.vue
@@ -0,0 +1,25 @@
+<script setup>
+	import Status from "./Status.vue";
+	import Date from "./Date.vue";
+	</script>
+	
+	<template>
+		<div class="card">
+		<Status />
+		<Date />
+	</div>
+	</template>
+	
+	<style scoped lang="scss">
+	.card {
+		box-sizing: border-box;
+		height: 300px;
+		width: 495px;
+		background-color: #ffffff;
+		border: 1px solid #e5e5e5;
+		border-radius: 8px;
+		box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px,
+			rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
+	}
+	</style>
+	
---
+++ src/components/Status.vue
@@ -0,0 +1,50 @@
+<script setup>
+	const props = defineProps(['status']);
+</script>
+
+	<template>
+		<div class="status"></div>
+	</template>
+
+
+<style scoped>
+	.statusActive{
+		position: absolute;
+width: 37px;
+height: 15px;
+left: 389px;
+top: 251px;
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 500;
+font-size: 13px;
+line-height: 15px;
+color: #00DB99;
+	}
+	.statusHold{
+		position: absolute;
+left: 37.58%;
+right: 52.93%;
+top: 35%;
+bottom: 60%;
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 500;
+font-size: 13px;
+line-height: 15px;
+color: #F0BB00;
+	}
+	.statusInactive{
+		position: absolute;
+width: 46px;
+height: 15px;
+left: 1435px;
+top: 251px;
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 500;
+font-size: 13px;
+line-height: 15px;
+color: #FF285C;
+	}
+</style>

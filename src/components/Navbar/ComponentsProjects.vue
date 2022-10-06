b4fa9871151fb3567b1b6deecbf2343295c81e80
Yunus Emre Topçu <yunusemre_topcu@hotmail.com>
2022-10-07 00:57:33 +0300

    navbar created

---
+++ src/components/Navbar/ComponentsProjects.vue
@@ -0,0 +1,17 @@
+<template>
+  <p class="projects">Projects</p>
+</template>
+
+<script setup>
+const projects = ref("Projects");
+</script>
+
+<style lang="scss" scoped>
+.projects {
+  font-family: "Roboto";
+  font-size: 24px;
+  font-weight: 500;
+  line-height: normal;
+  color: rgba(60, 85, 122, 1);
+}
+</style>

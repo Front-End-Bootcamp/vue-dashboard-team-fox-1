b17289d935b5d621e824cb6a324f03103911c628
Onur Çöpoğlu <onurcpgl.52@gmail.com>
2022-10-07 03:11:41 +0300

    css update

--- src/components/Card/Card.vue
+++ src/components/Card/Card.vue
@@ -1,45 +1,45 @@
 <script setup>
-import CardInfo from "./CardInfo.vue";
-import CardPictures from "./CardPictures.vue";
-import CardDate from "./CardDate.vue";
-import CardStatus from "./CardStatus.vue";
-import CardEditIcon from "./CardEditIcon.vue";
-import CardProgressBar from "./CardProgressBar.vue";
-
-const props = defineProps(["data"]);
-</script>
-
-<template>
-	<div class="card" v-for="item in props.data">
-		<CardEditIcon :title="item.title" />
-		<div class="status">
-			<CardStatus :status="item.status" />
-			<CardDate :date="item.startDate" />
+	import CardInfo from "./CardInfo.vue";
+	import CardPictures from "./CardPictures.vue";
+	import CardDate from "./CardDate.vue";
+	import CardStatus from "./CardStatus.vue";
+	import CardEditIcon from "./CardEditIcon.vue";
+	
+	const props = defineProps(["data"]);
+	</script>
+	
+	<template>
+		<div class="card" v-for="item in props.data">
+			<div class="title">
+				<CardEditIcon :title="item.title" />
+			</div>
+			<div class="status">
+				<CardStatus :status="item.status" />
+				<CardDate :date="item.startDate" />
+			</div>
+			<CardInfo :task="item.task" :user="item.user" />
+			<CardPictures :picture="item.membersPictures" />
 		</div>
-
-		<CardInfo :task="item.task" :cardUser="item.user" />
-		<CardPictures :picture="item.membersPictures" />
-
-		<div class="card_footer"></div>
-	</div>
-</template>
-
-<style scoped lang="scss">
-.card__footer {
-	position: absolute;
-}
-
-.card {
-	position: relative;
-	width: 0.75 * 495px;
-	height: 0.75 * 300px;
-	left: 0.75 * 345px;
-	top: 0.6 * 190px;
-	background-color: #ffffff;
-	border: 1px solid #e5e5e5;
-	border-radius: 8px;
-	box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px,
-		rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
-	margin: 8px;
-}
-</style>
+	</template>
+	
+	<style scoped lang="scss">
+	.status {
+		float: left;
+		text-align: center;
+	}
+	
+	.card {
+		position: relative;
+		width: 0.75 * 495px;
+		height: 0.75 * 300px;
+		left: 0.75 * 345px;
+		top: 0.6 * 190px;
+		background-color: #ffffff;
+		border: 1px solid #e5e5e5;
+		border-radius: 8px;
+		box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px,
+			rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
+		margin: 8px;
+	}
+	</style>
+	
--- src/components/Card/CardPictures.vue
+++ src/components/Card/CardPictures.vue
@@ -1,42 +1,42 @@
 <script setup>
-const props = defineProps(["picture"]);
-</script>
-
-<template>
-	<div class="members">
-		<p>Members</p>
-		<div>
-			<img
-				v-for="item in props.picture"
-				class="members_image"
-				v-bind:src="item"
-			/>
-			<img class="members_image" v-bind:src="'../../../public/added.png'" />
+	const props = defineProps(["picture"]);
+	</script>
+	
+	<template>
+		<div class="members">
+			<p >Members</p>
+			<div>
+				<img
+					v-for="item in props.picture"
+					class="members_image"
+					v-bind:src="item"
+				/>
+				<img class="members_image" v-bind:src="'../../../public/added.png'" />
+			</div>
 		</div>
-	</div>
-</template>
-
-<style scoped lang="scss">
-.members {
-	position: relative;
-	margin-top: 35%;
-	height: auto;
-	width: auto;
-	color: #3c557a;
-	margin-left: 5%;
-	margin-right: 5%;
-}
-.members > p {
-	margin-bottom: 2px !important;
-	margin-left: 2%;
-}
-
-.members_image {
-	width: 40px;
-	height: 40px;
-	border-radius: 50%;
-	margin-top: 2%;
-	object-fit: cover;
-	margin-left: 1%;
-}
-</style>
+	</template>
+	
+	<style scoped lang="scss">
+	.members {
+		position: relative;
+		margin-top: 26%;
+		height: auto;
+		width: auto;
+		color: #3c557a;
+		margin-left: 5%;
+		margin-right: 5%;
+	}
+	.members > p {
+		margin-bottom: 2px !important;
+		margin-left: 2%;
+	}
+	
+	.members_image {
+		width: 25px;
+		height: 25px;
+		border-radius: 50%;
+		object-fit: cover;
+		margin-left: 1%;
+	}
+	</style>
+	
